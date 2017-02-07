86ca375241a78dd6b8025b459bedcc808f26e556
TenchuHawke <hawke@unrestrainedgames.com>
2017-02-07 14:02:48 -0800

    First real commit

--- first_csharp/Program.cs
+++ first_csharp/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Now is the time for all good men to come to the aid of their countries.");
+            Console.WriteLine("the quick brown fox jumped over the lazy dog.");
         }
     }
 }
