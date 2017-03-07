04810684752fae39064eb7d3b6e8eca13e1dc7f2
TenchuHawke <hawke@unrestrainedgames.com>
2017-03-06 17:26:47 -0800

    Humans

---
+++ Human/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApplication {
+    public class Program {
+        public static void Main (string[] args) {
+            Human Bob = new Human ("Bob");
+            Human Lenny = new Human ("Lenny", 120, 4, 2, 1);
+            Bob.attack (Lenny);
+            string table = "";
+            Bob.attack (table);
+        }
+    }
+    public class Human {
+        public int Health;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        public string Name;
+
+        public Human (string name) {
+            Name = name;
+            Health = 100;
+            Dexterity = 3;
+            Intelligence = 3;
+            Strength = 3;
+            }
+        public Human (string name, int health, int strength, int dexterity, int intelligence) {
+            Name = name;
+            Health = health;
+            Strength = strength;
+            Dexterity = dexterity;
+            Intelligence = intelligence;
+        }
+        public void attack (object target) {
+            if (target is Human) {
+                // Human Target = target as Human;
+                target.Health -= (this.Strength * 5);
+                System.Console.WriteLine ("Your attack does " + (this.Strength * 5) + " damage to " + Target.Name);
+            } else {
+                System.Console.WriteLine ("Your Attack is ineffective on that target!");
+            }
+        }
+    }
+}
---
+++ Puzzles/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication {
+    public class Program {
+        public static void Main (string[] args) {
+            RandomArray();
+            TossCoin();
+            TossMultipleCoins(500);
+            string [] NameList = {"Todd", "Tiffany", "Charlie", "Geneva", "Sydney"};
+            string [] NamesOut = Names(NameList);
+
+            
+        }
+        public static int[] RandomArray () {
+            int[] Output = new int[10];
+            Random rand = new Random ();
+
+            for (int i = 0; i < 10; i++) {
+                Output[i] = rand.Next (5 , 25);
+            }
+            int Min = Output[0];
+            int Max = Output[0];
+            int Sum = 0;
+
+            for (int i = 0; i < 10; i++) {
+                if (Output[i] < Min) {
+                    Min = Output[i];
+                }
+                if (Output[i] > Max) {
+                    Max = Output[i];
+                }
+                Sum += Output[i];
+            }
+            System.Console.WriteLine ("Minimum: " + Min);
+            System.Console.WriteLine ("Maximum: " + Max);
+            System.Console.WriteLine ("Sum: " + Sum);
+            return Output;
+        }
+
+        public static string TossCoin () {
+            string Result = "";
+            Random rand = new Random ();
+            System.Console.WriteLine ("Tossing a Coin!");
+            if (rand.Next (0, 1) == 0) {
+                Result = "Heads";
+            } else {
+                Result = "Tails";
+            }
+            System.Console.WriteLine (Result + "!");
+            return Result;
+        }
+        public static double TossMultipleCoins (int num) {
+            Random rand = new Random ();
+            int count = 0;
+            for (int i = 0; i < num; i++) {
+                if (rand.Next (0, 1) == 0) {
+                    count++;
+                }
+            }
+            return (double) count/num;
+        }
+        public static string [] Names (string [] nameList)
+        {
+            List <string> OutputList = new List<string>();
+            for (int i = 0; i < nameList.Length; i++)
+            {
+                if (nameList[i].Length>5)
+                {
+                    OutputList.Add(nameList[i]);
+                }
+            }
+            int [] randArray = new int[OutputList.Count];
+            for (int i = 0; i < randArray.Length; i++)
+            {
+                randArray[i]=i;
+            }
+            int temp = 0;
+            int index = 0;
+            Random rand = new Random();
+            for (int i = 0; i < randArray.Length; i++)
+            {
+                index = rand.Next(0, randArray.Length);
+                temp = randArray[i];
+                randArray[i] = randArray[index];
+                randArray[index] = temp;
+            }
+            int j = 0;
+            string [] Output = new string [OutputList.Count];
+            foreach (string item in OutputList)
+            {
+                Output[randArray[j]]=item;
+                j++;
+            }
+            return Output;
+        }
+    }
+}
